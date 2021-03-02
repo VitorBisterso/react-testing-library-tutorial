65b5cccdb742bee0b6b535c4c752c8055b991a6f
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-03-02 14:37:05 -0300

    Add code from video

---
+++ src/components/Todo.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render, waitFor, fireEvent } from '@testing-library/react'
+
+import Todo from './Todo'
+
+describe('Tests for Todo component', () => {
+  it('Should add new task when form has been submitted', async () => {
+    const { getByTestId, getByText } = render(<Todo />)
+
+    const fieldNode = await waitFor(() => getByTestId('form-field'))
+    const newTask = 'testing'
+    fireEvent.change(
+      fieldNode,
+      { target: { value: newTask } }
+    )
+    expect(fieldNode.value).toEqual(newTask)
+
+    const btnNode = await waitFor(() => getByTestId('form-btn'))
+    fireEvent.click(btnNode)
+
+    const tdNode = await waitFor(() => getByText(newTask))
+    expect(tdNode).toBeDefined()
+  })
+})
